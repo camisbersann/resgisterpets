edb8bc669dc6e0f7f2758b1ab31cf209356d5ff6
camilabersan <camilabersan.564@gmail.com>
2023-08-28 08:16:35 -0300

    feat: create createpet function and fix errors

--- assets/js/script.registerpets.js
+++ assets/js/script.registerpets.js
@@ -17,3 +17,23 @@ class ListPet{
         this.pets.push(pet);
     }
 }
+
+const libraryPet= new ListPet();
+
+function createPet(){
+    let tutor = document.getElementById("input-tutor").value;
+    let petname= document.getElementById("input-petname").value;
+    let species = document.getElementById("input-species").value;
+    let picture = document.getElementById("input-picture").value;
+    let birthdate = document.getElementById("input-birthdate").value;
+
+    const pet = new Pet(tutor, petname, species, picture, birthdate);
+
+    console.log(pet);
+
+    libraryPet.add(pet);
+
+    showPet();
+
+
+}
